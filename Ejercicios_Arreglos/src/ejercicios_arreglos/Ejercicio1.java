b3db66c358b69912fbcd0f8989941e856cdefdb6
unknown <alisonvicab75@gmail.com>
2018-05-31 10:11:40 -0500

    Arreglo

---
+++ Ejercicios_Arreglos/src/ejercicios_arreglos/Ejercicio1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios_arreglos;
+
+/**
+ *
+ * @author Victoria
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        int[] c = new int[4];
+        c[0] = 10;
+        System.out.println(c.length);
+        for (int contador = 0; contador < c.length; contador++) {
+            System.out.println(c[contador]);
+        }
+
+    }
+
+}
